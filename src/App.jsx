e3a22663d6d249d150c6f55dd6285493e8c4bf12
BilalSangari <bilalsangar2003@gmail.com>
2026-01-04 20:38:59 +0430

    Search Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Search from './components/Search.jsx';
+
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('')
   return (
     <>
      <main>
@@ -8,8 +11,12 @@ const App = () => {
 
        <div className="wrapper">
         <header>
+          <img src="./hero.png" alt="Hero Banner" />
           <h1>Find <span className='text-gradient'>Movies</span> you'll Enjoy Without the Hussle</h1>
         </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <h1 className='text-white'>{searchTerm}</h1>
        </div>
      </main>
     </>
